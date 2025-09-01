d508347b9dbe53e705c3f89377e1766ac170321f
AruChard <richard.darmanto@gmail.com>
2025-09-01 12:52:47 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main(){
+             int age=25;
+             printf ("age:%d",age);
+             
+             
+             int newage=age-3;
+             printf("\nnew:%d",newage);
+             return 0 ;}
+             
+  
+
